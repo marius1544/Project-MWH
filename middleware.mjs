271b9b1100a0cd4b3cc97a709451507f9fe17bad
Marius <mariuswha@outlook.com>
2026-01-25 19:14:55 +0100

    added if check on fileType

--- middleware.mjs
+++ middleware.mjs
@@ -1,11 +1,12 @@
 let fileEndings = ['jpg', 'jpeg', 'ppx', 'gif', 'docx', 'xlxs', 'png', 'exe']
-for (let i = 0; i < fileEndings.length; i++) {
-    const file = fileEndings[i];
-}
 
 export function validateFileType(req, res, next){
     const fileType = req.body.filename
 
+    if(!fileType){
+        console.log("Feil: Mangler filnavn i body.");
+        return res.status(400).send("Bad Request: Missing 'filename' property in JSON body.");
+    }
     const fileEnding = fileType.split('.').pop();
      const isValid = fileEndings.find(item => item === fileEnding);
 
