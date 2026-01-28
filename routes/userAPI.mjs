b0cbc13bf2ac443c011fd59187db473b3f94316c
Marius <mariuswha@outlook.com>
2026-01-28 10:13:37 +0100

    Cleaned up delete and made it possible to delete user

--- routes/userAPI.mjs
+++ routes/userAPI.mjs
@@ -1,8 +1,7 @@
 import express from "express";
 import { generateID } from "../dataObjects/user.mjs";
 import user from "../dataObjects/user.mjs";
-import { generateUsername } from "../dataObjects/user.mjs";
-
+import { generateUsername, Users} from "../dataObjects/user.mjs";
 const userRouter = express.Router();
 userRouter.use(express.json())
 
@@ -11,16 +10,22 @@ userRouter.post('/', (req, res) => {
   newUser.id = generateID(); 
   newUser.username = "";
   
- if(newUser.username === ""){
-  newUser.username = generateUsername("Marius");
-  } else {
-  console.log("After if", newUser.username = "");
-}
+ if(!newUser.username){
+    newUser.username = generateUsername("Mariu");
+ }
+  Users[newUser.id] = newUser;
   res.json(JSON.stringify(newUser));
 })
 
-userRouter.delete('/', (req, res) => {
- res.json(JSON.stringify(newUser));
-})
+userRouter.delete('/:id', (req, res) => {
+  const id = req.params.id;
+  if(Users[id]){
+    delete Users[id];
+    res.status(200).json(JSON.stringify(`User ${id} deleted.`));
+  } else {
+    res.status(404).json(JSON.stringify(`User not found are`))
+  }
+});
+
 
 export default userRouter;
