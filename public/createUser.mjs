dd9c60cbb6cc05e08a945533648b79668f3bcf20
Marius <mariuswha@outlook.com>
2026-02-04 13:49:12 +0100

    Added UserResponse function to single fetch call

--- public/createUser.mjs
+++ public/createUser.mjs
@@ -1,3 +1,4 @@
+ let formOutPutFieldVar = document.createElement("div");
 export class CreateUserClass extends HTMLElement {
   constructor() {
     super();
@@ -13,16 +14,19 @@ export class CreateUserClass extends HTMLElement {
         <input id="TOSmenuCheckbox" type="checkbox" />
         <label for=TOSmenuCheckbox">Click to agree to <a href="./utils-folder/ToS.md">Terms of service</a>
         </label>
-         <button id="submitTOS">Submit</button>
-            <p id="formOutPutField"></p>        
+         <button id="submitTOS">Submit</button>     
             `;
-    this.appendChild(createUser);
+     
+formOutPutFieldVar.innerHTML = `<p id="formOutPutField"></p>`;
+
+  this.appendChild(createUser);
+  this.appendChild(formOutPutFieldVar);
   }
 
+  
   createUserCallback() {
     const TOSmenuCheckboxVar = document.getElementById("TOSmenuCheckbox");
     const userNameInp = document.getElementById("usernameInp");
-    let formOutPutFieldVar = document.getElementById("formOutPutField");
     const submitTosBtn = document.getElementById("submitTOS");
 
     submitTosBtn.disabled = true;
@@ -42,22 +46,54 @@ export class CreateUserClass extends HTMLElement {
       }
 
       try {
-        const response = await fetch("/user", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            username: usernameVar,
-            consent: hasConsented,
-          }),
-        });
-        const data = await response.json();
-        console.log(data);
+       
+        const userResponse = UserResponse().createUser("POST", usernameVar, hasConsented);
+        const data = await userResponse;
+
         formOutPutFieldVar.innerHTML = `User ${data}`;
-      } catch {
-        console.log("Feil ved opprettelse av bruker:");
+        console.log(data)
+      } catch (err) {
+        formOutPutFieldVar.innerHTML = `Feil ved opprettelse av bruker`, console.log(err);
       }
     });
   }
+
+deleteUserCallback(){
+}
+}
+
+
+
+
+
+function UserResponse (){
+   async function createUser(method, username, consent){
+    const response = await fetch(`/user/`, { 
+      method: `${method}`,
+    headers: {
+            "Content-Type": "application/json",
+          },
+    body: JSON.stringify({
+            username,
+            consent,
+          }),
+       });
+
+       try {
+    const data = await response.json();
+
+    if (!response.ok) {
+        throw new Error(data.message || "Server error");
+      }
+
+     return data;
+      
+       }catch(err){
+        this.formOutPutFieldVar.innerHTML = `Error ${err}`;
+        throw err;
+       }
+  }
+
+
+  return { createUser }
 }
