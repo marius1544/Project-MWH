e3e37becbdaeaeffd4bb3296b2bef9acddc71ed9
Marius <mariuswha@outlook.com>
2026-02-06 14:53:13 +0100

    Moved editUser to new document and changed callback

---
+++ public/editUser.mjs
@@ -0,0 +1,35 @@
+import { UserFunction } from "./utils-folder/utils.mjs";
+
+export class EditUserClass extends HTMLElement {
+    constructor() {
+    super();
+}
+
+connectedCallback() {
+    const submitUsernameBtn = document.getElementById("submitUsernameBtn");
+    const idInputFieldVar = document.getElementById("idInputField");
+    const newUsernameInputFieldVar = document.getElementById(
+      "newUsernameInputField",
+    );
+
+    submitUsernameBtn.addEventListener("click", async () => {
+      try {
+        const response = await UserFunction({
+          method: "PUT",
+          userID: idInputFieldVar.value,
+          username: newUsernameInputFieldVar.value,
+        });
+
+        if(response){
+        alert(
+          `Successfully updated your username to ${newUsernameInputFieldVar.value}`,
+        );
+    }else{
+        alert(`Can't find id: ${userID}`)
+    }
+      } catch (err) {
+        console.log("Feil ved oppdatering av brukernavn:", err);
+      }
+    });
+  }
+}
