25ccf85ad7726af108daa2b0883ce0d21ddde6a8
Marius <mariuswha@outlook.com>
2026-02-06 10:25:04 +0100

    Added editUserCallback

--- public/settingsClass.mjs
+++ public/settingsClass.mjs
@@ -5,31 +5,41 @@ export class SettingsClass extends HTMLElement {
     const Settings = document.createElement("div");
 
     Settings.innerHTML = `
-      <p>
-        Read our privacy policy
-        <a href="./utils-folder/Privacy-policy.md">here</a>
-      </p>
-    <br />
-        <div id="Settings">
-      <img
-        id="settings-gear"
-        src="./utils-folder/settings-gear.png"
-        alt="settings-gear"
-      />
-    </div>
-    <div class="sub-menu">
-      <div class="settingsClass" value="toggle-theme">
-        ☀️ / 🌙 Dark / Light mode
-      </div>
-      <div id="retract-consent" class="settingsClass" value="retract-delete">
-        ❌ Retract consent & delete account
-      </div>
-      <div class="input-div-sub-menu">
-      <input id="userIdInp" type="text" placeholder="Input your ID here" />
-      <button id="retract-delete">Retract</button>
-    </div>   
-    </div>       
-            `;
+    <p>
+  Read our privacy policy
+  <a href="./utils-folder/Privacy-policy.md">here</a>
+</p>
+
+<br />
+
+<div id="Settings">
+  <img
+    id="settings-gear"
+    src="./utils-folder/settings-gear.png"
+    alt="settings-gear"
+  />
+</div>
+
+<div class="sub-menu">
+  <button class="settingsClass">☀️ / 🌙 Dark / Light mode</button>
+
+  <button id="retract-consent" class="settingsClass">
+    ❌ Retract consent & delete account
+  </button>
+
+  <button id="edit-user" class="settingsClass">Edit user</button>
+
+  <div class="editUserDiv">
+    <input id="idInputField" placeholder="Enter id" />
+    <input id="newUsernameInputField" placeholder="Enter new username" />
+    <button id="submitUsernameBtn" type="button">Submit</button>
+</div>
+
+  <div class="input-div-sub-menu">
+    <input id="userIdInp" type="text" placeholder="Input your ID here" />
+    <button id="retract-delete">Retract</button>
+  </div>
+    `;
 
     this.appendChild(Settings);
   }
@@ -39,13 +49,18 @@ export class SettingsClass extends HTMLElement {
     const subMenu = this.querySelector(".sub-menu");
     const retractConsent = document.getElementById("retract-consent");
     const inputDivSubMenu = this.querySelector(".input-div-sub-menu");
-
+    const editUser = document.getElementById("edit-user");
+    const editUserDiv = this.querySelector(".editUserDiv");
     settingsIcon.addEventListener("click", () => {
       subMenu.classList.toggle("open");
     });
 
     retractConsent.addEventListener("click", () => {
       inputDivSubMenu.classList.toggle("open");
+    });
+
+    editUser.addEventListener("click", () => {
+      editUserDiv.classList.toggle("open");
     });
   }
 
@@ -65,6 +80,29 @@ export class SettingsClass extends HTMLElement {
         alert(data, userIdInp.value);
       } catch (err) {
         console.log("Feil ved sletting av bruker:", err);
+      }
+    });
+  }
+
+  editUserCallback() {
+    const submitUsernameBtn = document.getElementById("submitUsernameBtn");
+    const idInputFieldVar = document.getElementById("idInputField");
+    const newUsernameInputFieldVar = document.getElementById(
+      "newUsernameInputField",
+    );
+
+    submitUsernameBtn.addEventListener("click", async () => {
+      try {
+        const response = await UserFunction({
+          method: "PUT",
+          userID: idInputFieldVar.value,
+          username: newUsernameInputFieldVar.value,
+        });
+        alert(
+          `Successfully updated your username to ${newUsernameInputFieldVar.value}`,
+        );
+      } catch (err) {
+        console.log("Feil ved oppdatering av brukernavn:", err);
       }
     });
   }
