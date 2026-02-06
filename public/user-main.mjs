d33f17372703c158ba83d4481fcb9e0d4ea54ea2
Marius <mariuswha@outlook.com>
2026-02-06 15:46:05 +0100

    Removed loadData to comply with web component terminology

--- public/user-main.mjs
+++ public/user-main.mjs
@@ -1,27 +1,25 @@
 import { CreateUserClass } from "./createUser.mjs";
 import { SettingsClass } from "./settingsClass.mjs";
+import { EditUserClass } from "./editUser.mjs"
+import { DeleteUserClass } from "./deleteUser.mjs";
 customElements.define("create-user-view", CreateUserClass);
 customElements.define("user-settings-view", SettingsClass);
+customElements.define("edit-user-view", EditUserClass);
+customElements.define("delete-user-view", DeleteUserClass);
 const CreateUser = new CreateUserClass();
 const UserSettings = new SettingsClass();
+const EditUser = new EditUserClass();
+const DeleteUser = new DeleteUserClass();
+
 const viewMap = {
     "createuser": CreateUser,
     "usersettings": UserSettings,
+    "edituser": EditUser,
+    "deleteuser": DeleteUser
 }
 
 const pageContainer = document.getElementById("pageContainer");
 pageContainer.appendChild(CreateUser);
 pageContainer.appendChild(UserSettings);
-loadData()
-
-
-function loadData(){
-    try {
-        CreateUser.createUserCallback();
-        UserSettings.settingsMenuCallback();
-        UserSettings.retractDeleteCallback();
-        UserSettings.editUserCallback();
-    }catch(err){
-        console.log("Error", err);
-    }
-    }
+pageContainer.appendChild(EditUser);
+pageContainer.appendChild(DeleteUser);
