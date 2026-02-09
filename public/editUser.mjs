31dd9c9b67c8d20a21cc82ce79b93e7897504107
Marius <mariuswha@outlook.com>
2026-02-09 11:40:41 +0100

    Moved all related div elements to editUser.mjs

--- public/editUser.mjs
+++ public/editUser.mjs
@@ -1,11 +1,22 @@
 import { UserFunction } from "./utils-folder/utils.mjs";
 
 export class EditUserClass extends HTMLElement {
-    constructor() {
+  constructor() {
     super();
-}
 
-connectedCallback() {
+    const editDiv = document.createElement("div");
+    editDiv.innerHTML = `
+    <div class="editUserDiv">
+    <input id="idInputField" placeholder="Enter id" />
+    <input id="newUsernameInputField" placeholder="Enter new username" />
+    <button id="submitUsernameBtn" type="button">Submit</button>
+</div>
+`;
+
+    this.appendChild(editDiv);
+  }
+
+  connectedCallback() {
     const submitUsernameBtn = document.getElementById("submitUsernameBtn");
     const idInputFieldVar = document.getElementById("idInputField");
     const newUsernameInputFieldVar = document.getElementById(
@@ -20,13 +31,13 @@ connectedCallback() {
           username: newUsernameInputFieldVar.value,
         });
 
-        if(response){
-        alert(
-          `Successfully updated your username to ${newUsernameInputFieldVar.value}`,
-        );
-    }else{
-        alert(`Can't find id: ${userID}`)
-    }
+        if (response) {
+          alert(
+            `Successfully updated your username to ${newUsernameInputFieldVar.value}`,
+          );
+        } else {
+          alert(`Can't find id: ${userID}`);
+        }
       } catch (err) {
         console.log("Feil ved oppdatering av brukernavn:", err);
       }
