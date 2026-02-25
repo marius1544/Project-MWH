28f7ec7e93d923c8bf1e33f5fb310cd916eb5c3a
Marius <mariuswha@outlook.com>
2026-02-25 10:14:58 +0100

    Fix

--- server.mjs
+++ server.mjs
@@ -1,23 +1,21 @@
 import express from "express";
-import fileRouter from "./server-routes/check-file-api.mjs"
-import 'dotenv/config';
+import fileRouter from "./server-routes/check-file-api.mjs";
+import "dotenv/config";
 import userRouter from "./server-routes/userAPI.mjs";
 
-
 const PORT = process.env.PORT;
+const LocalPORT = process.env.Local_PORT;
 const app = express();
 
 app.use(express.json());
-app.use(express.static('public'))
+app.use(express.static("public"));
 
-app.use('/files', fileRouter);
-app.use('/user', userRouter)
-app.get('/', (req, res) => {
-  res.send('Hello Worlds');
-})
-
-
+app.use("/files", fileRouter);
+app.use("/user", userRouter);
+app.get("/", (req, res) => {
+  res.send("Hello Worlds");
+});
 
 app.listen(PORT, () => {
-  console.log('Server is running')
-})
+  console.log("Server is running");
+});
