e77c0f5b593ab5df528d91bf68a74282526b715f
Marius <mariuswha@outlook.com>
2026-02-26 13:54:42 +0100

    Made first step for postgreSQL database without params

--- db/dbfile.mjs
+++ db/dbfile.mjs
@@ -1,10 +1,9 @@
 import { client } from "./connection-sql.mjs"
-async function createUser(id, name) {
+async function createUser() {
   const result = await client.query(
     `INSERT INTO projectmwhtest (id, name)
      VALUES (9999993, 'Hansen')
      `,
-    [id, name]
   );
 
   return result.rows[0];
--- server.mjs
+++ server.mjs
@@ -2,13 +2,14 @@ import express from "express";
 import fileRouter from "./server-routes/check-file-api.mjs";
 import "dotenv/config";
 import userRouter from "./server-routes/userAPI.mjs";
+import { connectDB } from "./db/connection-sql.mjs";
 
-const PORT = process.env.PORT || process.env.Local_PORT || 3000;
+const PORT = process.env.Local_PORT || 3000;
 const app = express();
 
 app.use(express.json());
 app.use(express.static("public"));
-
+await connectDB()
 app.use("/files", fileRouter);
 app.use("/user", userRouter);
 app.get("/", (req, res) => {
