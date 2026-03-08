861e1226f085f332abac9e484233bcb50f19fa77
Marius Hansen <mariuswha@outlook.com>
2026-03-08 18:32:20 +0100

    Frontend site translation

---
+++ public/public-localization/i18n-frontend-loader.mjs
@@ -0,0 +1,29 @@
+let translations = {};
+
+export async function loadLanguage() {
+    const res = await fetch("/lang");
+    translations = await res.json();
+
+    const usernameInpes = document.querySelector("#usernameInp")
+    const submitBtn = document.querySelector("#submitTOS");
+    const privacypolicys = document.querySelector("#privacy-policy")
+    const privacylink = document.querySelector("#privacy-link")
+    const droptext = document.querySelector("#drop-text")
+    const clicktos = document.querySelector("#click-tos")
+    const toslink = document.querySelector("#tos-link")
+    const retractConsent = document.querySelector("#retract-consent")
+    const editUser = document.querySelector("#edit-user")
+    submitBtn.textContent = translations.submitButton;
+    privacypolicys.textContent = translations.privacypolicy
+    privacylink.textContent = translations.privacylink;
+    droptext.textContent = translations.droptext;
+    usernameInpes.placeholder = translations.usernameInp
+    clicktos.textContent = translations.clicktos
+    toslink.textContent = translations.toslink
+    retractConsent.textContent = translations.retractConsent
+    editUser.textContent = translations.editUser
+}
+
+export function getTranslations() {
+    return translations;
+}
