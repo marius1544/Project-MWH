9a4b9f20802c24c856d2187eede2f8df085fd03f
Marius Hansen <mariuswha@outlook.com>
2026-03-08 19:06:37 +0100

    Port bugs

--- server.mjs
+++ server.mjs
@@ -4,7 +4,7 @@ import "dotenv/config";
 import userRouter from "./server/server-routes/userAPI.mjs";
 import { connectDB } from "./db/connection-sql.mjs";
 import { getL10n } from "./server/services/getL10n.mjs";
-const PORT = 8081;
+const PORT = process.env.PORT || process.env.Local_PORT;
 const app = express();
 
 
