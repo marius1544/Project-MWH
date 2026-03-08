37453c9192644c996bc21482940ff6a03bfa1cf5
Marius Hansen <mariuswha@outlook.com>
2026-03-08 18:33:29 +0100

    Bug fixes

--- server.mjs
+++ server.mjs
@@ -1,19 +1,21 @@
 import express from "express";
-import fileRouter from "./server-routes/check-file-api.mjs";
+import fileRouter from "./server/server-routes/check-file-api.mjs";
 import "dotenv/config";
-import userRouter from "./server-routes/userAPI.mjs";
+import userRouter from "./server/server-routes/userAPI.mjs";
 import { connectDB } from "./db/connection-sql.mjs";
-
-const PORT = process.env.PORT || process.env.Local_PORT || 3000;
+import { getL10n } from "./server/services/getL10n.mjs";
+const PORT = process.env.Local_PORT ||8081;
 const app = express();
+
 
 app.use(express.json());
 app.use(express.static("public"));
 await connectDB()
 app.use("/files", fileRouter);
 app.use("/user", userRouter);
-app.get("/", (req, res) => {
-  res.send("Hello Worlds");
+app.get("/lang", (req, res) => {
+    const l10n = getL10n(req)
+    res.json(l10n);
 });
 
 app.listen(PORT, () => {
