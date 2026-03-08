bbb223cf27543c4af7d0289b06ebf579a7ec27da
Marius Hansen <mariuswha@outlook.com>
2026-03-08 18:32:52 +0100

    Added more urls for offline usage

--- public/service-worker/service-worker-install.mjs
+++ public/service-worker/service-worker-install.mjs
@@ -5,14 +5,32 @@ const urls = [
     "/index.html",
     "/app.mjs",
     "/app.css",
+    "/manifest.json",
+
     "/components/createUser.mjs",
     "/components/deleteUser.mjs",
     "/components/editUser.mjs",
     "/components/dropboxClass.mjs",
     "/components/settingsClass.mjs",
-     "/controllers/views.mjs",
+    "/controllers/views.mjs",
     "/controllers/viewLoader.mjs",
-    "/controllers/utils.mjs"
+
+    "/utils-folder/utils.mjs",
+    "/utils-folder/fetchreq.mjs",
+    "/utils-folder/Privacy-policy.md",
+    "/utils-folder/ToS.md",
+    "/utils-folder/settings-gear.png",
+
+    "/views/createuserView.html",
+    "/views/deleteuserView.html",
+    "/views/dropboxView.html",
+    "/views/edituserView.html",
+    "/views/settingsView.html",
+
+    "/public-localization/i18n-frontend-loader.mjs",
+    "/dropbox-background.jpg",
+    "/icons/logo.png"
+
 ]
 
 self.addEventListener("install", (event) => {
