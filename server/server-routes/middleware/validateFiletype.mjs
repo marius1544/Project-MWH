e6be76d24edac0d0a2f64bb042e48d569df1fdc3
Marius <mariuswha@outlook.com>
2026-03-09 15:29:21 +0100

    Made folder for middleware and changed filename

---
+++ server/server-routes/middleware/validateFiletype.mjs
@@ -0,0 +1,22 @@
+let fileEndings = ["jpg", "jpeg", "ppx", "gif", "docx", "xlxs", "png", "exe"];
+
+function validateFileType(req, res, next) {
+  const fileType = req.body.filename;
+  if (!fileType) {
+    return res
+      .status(400)
+      .json({
+        error: "Bad Request: Missing 'filename' property in JSON body.",
+      });
+  }
+  const fileEnding = fileType.split(".").pop();
+  const isValid = fileEndings.find((item) => item === fileEnding);
+
+  if (isValid) {
+    next();
+  } else {
+    return res.status(400).json({ error: "Invalid file type" });
+  }
+}
+
+export default validateFileType;
