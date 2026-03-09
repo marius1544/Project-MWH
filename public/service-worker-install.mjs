61dc5d8756b3ed3c450c52fe93368f98f9bf63eb
Marius <mariuswha@outlook.com>
2026-03-09 11:36:29 +0100

    Moved to root, fixed offline usage

---
+++ public/service-worker-install.mjs
@@ -0,0 +1,60 @@
+const cacheName = "my-site-cache";
+
+const urls = [
+    "/",
+    "/index.html",
+    "/app.mjs",
+    "/app.css",
+    "/manifest.json",
+
+    "/components/createUser.mjs",
+    "/components/deleteUser.mjs",
+    "/components/editUser.mjs",
+    "/components/dropboxClass.mjs",
+    "/components/settingsClass.mjs",
+    "/controllers/views.mjs",
+    "/controllers/viewLoader.mjs",
+
+    "/utils-folder/utils.mjs",
+    "/utils-folder/fetchreq.mjs",
+    "/utils-folder/Privacy-policy.md",
+    "/utils-folder/ToS.md",
+    "/utils-folder/settings-gear.png",
+
+    "/views/createuserView.html",
+    "/views/deleteuserView.html",
+    "/views/dropboxView.html",
+    "/views/edituserView.html",
+    "/views/settingsView.html",
+
+    "/public-localization/i18n-frontend-loader.mjs",
+    "/dropbox-background.jpg",
+    "/icons/logo.png",
+    "/service-worker-install.mjs",
+    "/service-worker.mjs",
+    "/lang"
+
+]
+
+self.addEventListener("install", (event) => {
+    event.waitUntil(
+        caches.open(cacheName).then((cache) => {
+            return cache.addAll(urls);
+        })
+    );
+});
+
+self.addEventListener("activate", () => {
+    console.log("Service worker activated");
+})
+
+self.addEventListener("fetch", (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            if (response) {
+                return response;
+            }
+            return fetch(event.request);
+        })
+    );
+});
---
+++ public/service-worker.mjs
@@ -0,0 +1,12 @@
+const registerServiceWorker = async () => {
+    if ("serviceWorker" in navigator) {
+        try {
+            const registration = await navigator.serviceWorker.register("/service-worker-install.mjs");
+            console.log("Service Worker registered:", registration);
+        } catch (error) {
+            console.error("Service Worker registration failed:", error);
+        }
+    }
+}
+
+export default registerServiceWorker;
