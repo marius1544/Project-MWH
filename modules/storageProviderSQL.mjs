ea625c69d7835768bf16fe178fa82f6e273837c9
Marius Hansen <mariuswha@outlook.com>
2026-03-12 18:27:13 +0100

    First phase in being able to convert your exported data to JSON or CSV

--- modules/storageProviderSQL.mjs
+++ modules/storageProviderSQL.mjs
@@ -1,11 +1,55 @@
+import user from "../dataObjects/user.mjs";
 import { client } from "../db/connection-sql.mjs"
 
-async function retrieveInfo(){
-const result = await client.query(
-    `
+export async function retrieveInfo() {
+    const result = await client.query(
+        `
     SELECT * FROM projectmwhtest
     `);
-     return result.rows[0];
+    return result.rows[0];
 };
 
-export default retrieveInfo;
+
+
+export async function getUsers() {
+    try {
+        const users = await retrieveInfo();
+        console.log(users);
+        console.log("first type is " + typeof (users));
+    } catch (error) {
+        console.error("The error is", error)
+    }
+}
+
+async function toJson() {
+    const users = await retrieveInfo();
+    const stringified = JSON.stringify(users)
+    console.log(stringified);
+    console.log("second string is " + typeof (stringified));
+}
+
+async function toCSV() {
+    const users = await retrieveInfo();
+    const csvRows = [];
+
+    const headers = Object.keys(users);
+    csvRows.push(headers.join(','));
+
+    const values = Object.values(users).join(',');
+    csvRows.push(values)
+
+    return csvRows.join('\n')
+
+}
+
+async function main() {
+    const csv = await toCSV();
+    console.log(csv);
+    console.log("last string is " + typeof (toCSV()));
+}
+
+
+
+getUsers();
+toJson();
+main();
