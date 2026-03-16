41698884c92906bb0fad2e292ca8a97ec5b53521
Marius <mariuswha@outlook.com>
2026-03-16 11:08:17 +0100

    Added exportRouter to server

--- server.mjs
+++ server.mjs
@@ -3,13 +3,17 @@ import "dotenv/config";
 import userRouter from "./server/server-routes/userAPI.mjs";
 import { connectDB } from "./db/connection-sql.mjs";
 import { getL10n } from "./server/services/getL10n.mjs";
-const PORT = process.env.PORT || process.env.Local_PORT || 3000;
+import { getUsers } from "./modules/storageProviderSQL.mjs";
+import exportRouter from "./server/server-routes/settingsAPI.mjs";
+const PORT = process.env.PORT || 3000; 
 const app = express();
 
 
 app.use(express.json());
 app.use(express.static("public"));
 await connectDB()
+await getUsers();
+app.use("/export", exportRouter)
 app.use("/user", userRouter);
 app.get("/lang", (req, res) => {
     const l10n = getL10n(req)
