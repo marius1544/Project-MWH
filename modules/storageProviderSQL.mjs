e23a15a058c4a03a92294c74fd838bdd1387faef
Marius <mariuswha@outlook.com>
2026-03-16 11:07:26 +0100

    Backend functionality for exporting data

--- modules/storageProviderSQL.mjs
+++ modules/storageProviderSQL.mjs
@@ -1,55 +1,40 @@
-import user from "../dataObjects/user.mjs";
 import { client } from "../db/connection-sql.mjs"
 
-export async function retrieveInfo() {
+export async function retrieveInfo(id) {
     const result = await client.query(
         `
-    SELECT * FROM projectmwhtest
-    `);
+    SELECT * FROM projectmwhtest where id = $1
+    `,
+[id]
+)
+    
     return result.rows[0];
 };
 
 
 
-export async function getUsers() {
+export async function getUsers(id) {
     try {
-        const users = await retrieveInfo();
+        const users = await retrieveInfo(id);
         console.log(users);
-        console.log("first type is " + typeof (users));
     } catch (error) {
-        console.error("The error is", error)
     }
 }
 
-async function toJson() {
-    const users = await retrieveInfo();
-    const stringified = JSON.stringify(users)
-    console.log(stringified);
-    console.log("second string is " + typeof (stringified));
+export async function toJson(id) {
+    const users = await retrieveInfo(id);
+    return users;
 }
 
-async function toCSV() {
-    const users = await retrieveInfo();
+export async function toCSV(id) {
+    const users = await retrieveInfo(id);
     const csvRows = [];
 
     const headers = Object.keys(users);
-    csvRows.push(headers.join(','));
+    csvRows.push(headers.join(","));
 
-    const values = Object.values(users).join(',');
-    csvRows.push(values)
+    const values = Object.values(users).join(",")
+    csvRows.push(values);
 
-    return csvRows.join('\n')
-
+    return csvRows.join('\n');
 }
-
-async function main() {
-    const csv = await toCSV();
-    console.log(csv);
-    console.log("last string is " + typeof (toCSV()));
-}
-
-
-
-getUsers();
-toJson();
-main();
