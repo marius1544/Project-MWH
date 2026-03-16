18181c58a5381f5eb1854b7a3c8bd405a4bb1b83
Marius <mariuswha@outlook.com>
2026-03-16 11:04:00 +0100

    Make app able to export to CSV and JSON with the help of userid

--- server/server-routes/settingsAPI.mjs
+++ server/server-routes/settingsAPI.mjs
@@ -1,16 +1,33 @@
 import express from "express";
 export const settingsRouter = express.Router();
-export const adminRouter = express.Router();
-
-adminRouter.get('/', (req, res) => {
-  res.send("Admin page");
+export const exportRouter = express.Router();
+import { retrieveInfo, toJson, toCSV } from "../../modules/storageProviderSQL.mjs";
+exportRouter.get('/json/:id', async (req, res) => {
+  try {
+    const id = req.params.id;
+    const jsonData = await toJson(id);
+    return res.status(200).json(jsonData);
+  } catch (error) {
+    return res.status(500).json({ error: "Could not export JSON" });
+  }
+});
   
-})
 
+exportRouter.get('/csv/:id', async (req, res) => {
+  try {
+    const id = req.params.id;
+    const csvData = await toCSV(id);
+    res.header('Content-Type', 'text/csv');
+res.attachment('data.csv');
+return res.status(200).send(csvData);
+  } catch (error) {
+    return res.status(500).json({ error: "Could not export CSV" });
+  }
+})
 
 settingsRouter.get('/', (req, res) => {
 })
 
 
 
-export default settingsRouter;
+export default exportRouter;
