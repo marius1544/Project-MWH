4d0e96258e33c436c927c4c5bced4539123bbb3e
Marius <mariuswha@outlook.com>
2026-03-26 12:30:53 +0100

    Reformatted i18n localization

--- public/public-localization/i18n-frontend-loader.mjs
+++ public/public-localization/i18n-frontend-loader.mjs
@@ -4,36 +4,47 @@ export async function loadLanguage() {
     const res = await fetch("/lang");
     translations = await res.json();
 
-    const usernameInpes = document.querySelector("#usernameInput")
+    const usernameInput = document.querySelector("#usernameInput");
     const submitBtn = document.querySelector("#submitTOS");
-    const privacypolicys = document.querySelector("#privacy-policy")
-    const privacylink = document.querySelector("#privacy-link")
-    const droptext = document.querySelector("#drop-text")
-    const clicktos = document.querySelector("#click-tos")
-    const toslink = document.querySelector("#tos-link")
-    const retractConsent = document.querySelector("#retract-consent")
-    const editUser = document.querySelector("#edit-user")
-    const filelabels = document.querySelector("#filelabel")
-    const exportUserdata = document.querySelector("#exportUserdata")
-    const settingsIdInput = document.querySelector("#settingsIdInput")
-    const exportButton = document.querySelector("#exportButton")
-    const submitUsernameBtn = document.querySelector("#submitUsernameBtn")
-    const retractButton = document.querySelector("#retract-delete")
-    submitBtn.textContent = translations.submitButton;
-    privacypolicys.textContent = translations.privacypolicy
-    privacylink.textContent = translations.privacylink;
-    droptext.textContent = translations.droptext;
-    usernameInpes.placeholder = translations.usernameInp
-    clicktos.textContent = translations.clicktos
-    toslink.textContent = translations.toslink
-    retractConsent.textContent = translations.retractConsent
-    editUser.textContent = translations.editUser
-    filelabels.textContent = translations.filelabel
-    exportUserdata.textContent = translations.exportUserdata
-    settingsIdInput.placeholder = translations.settingsIdInput
-    exportButton.textContent = translations.exportButton
-    submitUsernameBtn.textContent = translations.submitButton
-    retractButton.textContent = translations.retractButton
+    const privacyPolicy = document.querySelector("#privacy-policy");
+    const privacyLink = document.querySelector("#privacy-link");
+    const dropText = document.querySelector("#drop-text");
+    const clickTos = document.querySelector("#click-tos");
+    const tosLink = document.querySelector("#tos-link");
+    const retractConsent = document.querySelector("#retract-consent");
+    const editUser = document.querySelector("#edit-user");
+    const fileLabel = document.querySelector("#filelabel");
+    const exportUserdata = document.querySelector("#exportUserdata");
+    const settingsIdInput = document.querySelector("#settingsIdInput");
+    const exportButton = document.querySelector("#exportButton");
+    const submitUsernameBtn = document.querySelector("#submitUsernameBtn");
+    const retractButton = document.querySelector("#retract-delete");
+    const adminPageButton = document.querySelector("#goToAdminPage");
+
+    function translate(element, text) {
+          element.textContent = text;
+    }
+
+    function translatePlaceholder(element, text) {
+          element.placeholder = text;
+    }
+
+    translatePlaceholder(usernameInput, translations.frontend.usernameInp);
+    translate(submitBtn, translations.frontend.submitButton);
+    translate(privacyPolicy, translations.frontend.privacypolicy);
+    translate(privacyLink, translations.frontend.privacylink);
+    translate(dropText, translations.frontend.droptext);
+    translate(clickTos, translations.frontend.clicktos);
+    translate(tosLink, translations.frontend.toslink);
+    translate(retractConsent, translations.frontend.retractConsent);
+    translate(editUser, translations.frontend.editUser);
+    translate(fileLabel, translations.frontend.filelabel);
+    translate(exportUserdata, translations.frontend.exportUserdata);
+    translatePlaceholder(settingsIdInput, translations.frontend.settingsIdInput);
+    translate(exportButton, translations.frontend.exportButton);
+    translate(submitUsernameBtn, translations.frontend.submitButton);
+    translate(retractButton, translations.frontend.retractButton);
+    translate(adminPageButton, translations.frontend.adminPageButton);
 }
 
 export function getTranslations() {
